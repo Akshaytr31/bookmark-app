8a3246337afda776ceb0e6b28b02ae8b249d5033
akshay tr <akshaytr31@gmail.com>
2026-02-18 14:50:09 +0530

    fix :deployment issue

--- app/auth/actions.ts
+++ app/auth/actions.ts
@@ -4,12 +4,14 @@ import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import { createClient } from '@/utils/supabase/server'
 
+import { getURL } from '@/utils/get-url'
+
 export async function login() {
   const supabase = await createClient()
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL ?? 'http://localhost:3000'}/auth/callback`,
+      redirectTo: `${getURL()}auth/callback`,
     },
   })
 
